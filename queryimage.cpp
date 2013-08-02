216178d4245e1203000b08102659fc830a769c43
Ghilardi Davide <ghilardi.davide@gmail.com>
2013-08-02 18:51:20 +0200

    Aggiunte shape e location feature
    Problema: così compila ma c'è una parte commentata che servirebbe ma lo
    fa crashare per problemi con la memoria...

--- queryimage.cpp
+++ queryimage.cpp
@@ -4,16 +4,25 @@
 
 QueryImage::QueryImage(string filename): image(filename) {
     IplImage iplimg = *(image.getImage());
+    vector<vector<Pixel> > superPixels;
     segmentImage(&iplimg, superPixels);
 
     cv::Mat segmented(image.getImage()->size(), CV_8UC1);
     int nSp = superPixels.size();
     for(int i=0; i<nSp; ++i){
+        superPixelList.push_back(new SuperPixel(superPixels[i], *image.getImage()));
         for(int j=0; j<superPixels[i].size(); ++j){
             segmented.at<uchar>(superPixels[i][j].y, superPixels[i][j].x) = 255*i/nSp;
         }
     }
+    //cout << "Distance: "<<superPixelList[3]->getMaskDistance(superPixelList[0]->getMaskFeature()) << endl;
 
     cvNamedWindow("superPixels",2);
     imshow("superPixels",segmented);
+}
+
+QueryImage::~QueryImage(){
+    for(int i=0; i<superPixelList.size(); ++i){
+        delete superPixelList[i];
+    }
 }
