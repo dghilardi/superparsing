c08f5108f983541464b8b1e1bfc3942ac42ff028
Ghilardi Davide <ghiladeb@debiantesting.oceano>
2013-08-13 09:21:37 +0200

    Modifiche al calcolo delle soglie

--- retrievalset.cpp
+++ retrievalset.cpp
@@ -11,12 +11,14 @@ RetrievalSet::RetrievalSet(vector<string> &names) : imgNames(names), matchResult
 void RetrievalSet::LabelImg(QueryImage &imgToLabel){
     //Recupero il vettore di superpixel della query image
     vector<SuperPixel *> *setSuperPixelsToLabel = imgToLabel.getSuperPixels();
-    //Ad ogni superpixel assegno la migliore feature che lo descrive
+    //Faccio scorrere i superpixels del retrieval set e della query image, provo i match e salvo le statistiche
     for(uint i=0; i<setSuperPixelsToLabel->size(); ++i) matchResults.push_back(new GlobLikelihood());
     //Elaboro le immagini del retrieval set
     for(uint i=0; i<imgNames.size(); ++i){
         //Recupero i superpixel della i-esima immagine del retrieval set
         RetrImage setImage(imgNames[i]);
+        //setImage.show();
+        //cv::waitKey();
         vector<SuperPixel *> *setImgSuperPixels = setImage.getSuperPixels();
         for(uint j=0; j<setImgSuperPixels->size(); ++j){
             SuperPixel *setSuperPixel = (*setImgSuperPixels)[j];
@@ -47,6 +49,7 @@ void RetrievalSet::LabelImg(QueryImage &imgToLabel){
     }
     imgToLabel.showSrc();
     imgToLabel.showLabeling();
+    cout << "ERRORE: " << imgToLabel.checkResults()*100 << "%\n";
     //Libero la memoria
     for(int i=0; i<matchResults.size(); ++i) delete matchResults[i];
 }
@@ -62,6 +65,8 @@ void RetrievalSet::checkSuperPixel(SuperPixel *toLabel, SuperPixel *inSet, GlobL
     double tk_mask=15, tk_relH=0.15, tk_sift=0.1, tk_color=0.1; //Valori casuali solo per provare
 
     int actualLabel = inSet->getLabel(); //Indice del label del superpixel preso dal dataset
+    //cout << actualLabel << endl;
+    if(actualLabel==9) throw;
 
     double maskDistance = toLabel->getMaskDistance(*inSet);
     if(maskDistance<tk_mask) spixelResults.mask.foundMatch(actualLabel);
@@ -74,12 +79,12 @@ void RetrievalSet::checkSuperPixel(SuperPixel *toLabel, SuperPixel *inSet, GlobL
     //else cout << "No distance" << endl;
 
     double siftDistance = toLabel->getSiftDistance(*inSet);
-    if(siftDistance<tk_sift) spixelResults.quantSIFT.foundMatch(actualLabel);
+    if(1-siftDistance<tk_sift) spixelResults.quantSIFT.foundMatch(actualLabel);
     //if(abs(siftDistance)<tk_sift) cout << "found sift: "<< inSet->getLabel() << endl;
     //else cout << "No SIFT" << endl;
 
     double colDistance = toLabel->getColorDistance(*inSet);
-    if(colDistance<tk_color) spixelResults.colorHist.foundMatch(actualLabel);
+    if(1-colDistance<tk_color) spixelResults.colorHist.foundMatch(actualLabel);
     //if(abs(colDistance)<tk_color) cout << "found color: " << inSet->getLabel() << endl;
     //else cout << "No Color" << endl;
 }
