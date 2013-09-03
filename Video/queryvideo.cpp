f30b2705ec58283c4e187c1f9f79e5118206a9d3
Ghilardi Davide <ghilardi.davide@gmail.com>
2013-09-03 11:32:03 +0200

    Sistemato leak

--- Video/queryvideo.cpp
+++ Video/queryvideo.cpp
@@ -1,12 +1,9 @@
 #include "queryvideo.h"
 
 QueryVideo::QueryVideo(string path): video(path){
-    vector<IplImage *> frames;
+    vector<cv::Mat> frames;
     vector<vector<Pixel> > superPixels;
 
     video.getImages(frames, 25);
     segmentVideo(frames, superPixels);
-
-    for(int i=0; i<frames.size(); ++i) delete frames[i];
-    frames.clear();
 }
--- Video/video.cpp
+++ Video/video.cpp
@@ -3,13 +3,11 @@
 Video::Video(string path) : vid(path){
 }
 
-void Video::getImages(vector<IplImage *> &result, int maxNumber){
+void Video::getImages(vector<cv::Mat> &result, int maxNumber){
     assert(result.empty());
     cv::Mat image;
     for(int i=0; i<maxNumber; ++i){
         vid >> image;
-        IplImage *tmpimg = new IplImage(image);
-        result.push_back(cvCloneImage(tmpimg));
-        delete tmpimg;
+        result.push_back(image.clone());
     }
 }
